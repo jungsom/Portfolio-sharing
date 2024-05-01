845c8c81d8b4bbf6949fa1c3cd24b83820e447d4
civica23@gmail.com <civica23@gmail.com>
2024-05-01 15:03:54 +0900

    fix : error 코드 수정

--- index.js
+++ index.js
@@ -67,20 +67,6 @@ app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/education", educationRouter);
 
-// 등록되지 않은 path로 요청이 오면 404 에러 발생
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-  res.render("error");
-});
-
 app.listen(process.env.PORT || 3000, () => {
   console.log(`서버가 ${process.env.PORT}번 포트에서 시작되었습니다.`);
 });
