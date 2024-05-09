58943248a29d4640604acad0a5870501cf286524
[AI10] 정유경 <jungyk0414@naver.com>
2024-05-09 13:55:05 +0000

    메인페이지 최종 스타일 및 게시판 경로 생성

--- views/network/network.js
+++ views/network/network.js
@@ -232,7 +232,6 @@ async function updateMenu() {
     userpageElem.style.display = "none";
     logoutElem.style.display = "none";
     loginElem.style.display = "block";
-    joinElem.style.display = "block";
   }
 }
 
@@ -279,16 +278,8 @@ async function init() {
   const boardElem = document.querySelector(".menu-items.board");
   const aboutElem = document.querySelector(".menu-items.aboutUs");
 
-<<<<<<< HEAD
   loginElem.addEventListener("click", () => {
     window.location.href = "/login";
-=======
-  loginElem.addEventListener("click", menuClickHandler);
-  userpageElem.addEventListener("click", menuClickHandler);
-
-  boardElem.addEventListener("click", () => {
-    window.location.href = "/board/?page=1";
->>>>>>> 3f785f52660b834df581080af376aaa9041f3aba
   });
   userpageElem.addEventListener("click", goToUserPage);
   logoutElem.addEventListener("click", () => {
